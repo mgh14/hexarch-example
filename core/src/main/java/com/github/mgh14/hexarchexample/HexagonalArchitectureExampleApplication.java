8576e5c7de32889565010ffb20ab12a369ffa6d9
mgh14 <chasing314@gmail.com>
2020-01-29 22:45:51 -0700

    Initial project structure commit

---
+++ core/src/main/java/com/github/mgh14/hexarchexample/HexagonalArchitectureExampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.mgh14.hexarchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HexagonalArchitectureExampleApplication.class, args);
+  }
+}
---
+++ core/src/test/java/com/github/mgh14/hexarchexample/HexagonalArchitectureExampleApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.mgh14.hexarchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureExampleApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
